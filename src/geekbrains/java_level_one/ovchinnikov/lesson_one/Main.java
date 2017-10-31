61d1d6826d12850903af41c1ad65c5b6641e3600
Ivan <2622610@gmail.com>
2017-10-31 16:17:18 +0300

    Старт пакетной структуры проекта

--- src/geekbrains/java_level_one/ovchinnikov/lesson_one/Main.java
+++ src/geekbrains/java_level_one/ovchinnikov/lesson_one/Main.java
@@ -2,7 +2,11 @@ package geekbrains.java_level_one.ovchinnikov.lesson_one;
 
 public class Main {
 
+    int a;
+    int A;
+
     public static void main(String[] args) {
-	// write your code here
+	byte b = 0;
+	short sh = 1 + 2;
     }
 }
