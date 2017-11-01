7411403bb8c49898c0f3354a84725d17f7428228
Ivan <2622610@gmail.com>
2017-11-01 07:49:32 +0300

    Старт пакетной структуры проекта

--- src/geekbrains/java_level_one/ovchinnikov/lesson_one/Main.java
+++ src/geekbrains/java_level_one/ovchinnikov/lesson_one/Main.java
@@ -8,5 +8,14 @@ public class Main {
     public static void main(String[] args) {
 	byte b = 0;
 	short sh = 1 + 2;
+	int i = 0;
+	long l = 4000000000000L;
+
+	float f = 5.124543413f;
+	double d = 5.12345432;
+
+	char c = 'A';
+
+	System.out.println();
     }
 }
