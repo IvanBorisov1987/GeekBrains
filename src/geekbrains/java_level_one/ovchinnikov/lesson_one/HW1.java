1db3511ab27ffe82e4a4843268fb3f23222eb587
PillarDevelopment <2622610@gmail.com>
2018-02-08 10:15:57 +0300

    reload

---
+++ src/geekbrains/java_level_one/ovchinnikov/lesson_one/HW1.java
@@ -0,0 +1,26 @@
+package geekbrains.java_level_one.ovchinnikov.lesson_one;
+
+public class HW1 {
+
+    int a;
+    int A;
+
+        //  1 Прописать метод main
+
+    public static void main(String[] args) {
+
+        // 2 Создать переменные всех типов и инициализировать их значения
+	byte b = 0;                 // целое число от -128 до 127
+	short sh = 1 + 2;           // целое число от -32768 до 32767
+	int i = 0;                  // целое число от -2147483648 до 2147483647
+	long l = 4000000000000L;    // число с плавающей запятой
+
+	float f = 5.124543413f;     // число с плавающей запятой
+	double d = 5.12345432;      // число с остатком от деления
+
+	char c = 'A';               //символьный тип
+    String str = "Hello world"; // Любое количество символов в юникоде
+    boolean flstr = true;       // логическое значение типа false или true
+
+    }
+}
